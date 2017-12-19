e8cbfe086a38f2b70a2182a211269a431ffd4fcb
Mika Tammi <mikatammi@gmail.com>
2017-12-19 16:42:07 +0200

    Also log console output into a file

--- market_maker/utils/log.py
+++ market_maker/utils/log.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 from market_maker.settings import settings
 
 
@@ -8,7 +9,12 @@ def setup_custom_logger(name, log_level=settings.LOG_LEVEL):
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
 
+    datetimestring = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    filehandler = logging.FileHandler("%s-%s.log" % (name, datetimestring))
+    filehandler.setFormatter(formatter)
+
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
     logger.addHandler(handler)
+    logger.addHandler(filehandler)
     return logger
